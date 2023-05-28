b27f43b0f00384838999ad00c1648edcba27cabf
Phillip Weston <phillip@async.design>
2023-05-28 13:33:50 -0400

    update to save phone

--- routes/tickets.js
+++ routes/tickets.js
@@ -30,8 +30,8 @@ module.exports = ({ psql, knex }) => {
   }
 
   async function createTicket (ctx, next) {
-    const { name, email } = ctx.request.body
-    const user = await User.query().insert({ name, email })
+    const { name, email, phone } = ctx.request.body
+    const user = await User.query().insert({ name, email, phone })
     await Ticket.query().insert({ user_id: user.user_id, original_owner_id: user.user_id })
     await Audit.query().insert({
       action: 'create',
